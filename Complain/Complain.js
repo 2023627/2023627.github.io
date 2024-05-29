2e7d4a0504e3f53372a29015b9aad6b1f22d3a53
2023627 <144215275+2023627@users.noreply.github.com>
2024-05-29 03:22:08 -0700

    Complain.js

--- Complain/Complain.js
+++ Complain/Complain.js
@@ -1,11 +1,12 @@
+//java script is used to creat a button when pused by user it comes with success meassage
 document.addEventListener('DOMContentLoaded', (event) => {
     const form = document.getElementById('complain-form');
     const successMessage = document.getElementById('success-message');
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the form from submitting in the traditional way
-        successMessage.style.display = 'block'; // Show the success message
-        form.reset(); // Clear the form fields
+        event.preventDefault(); // Stops the form from submitting in the traditional way
+        successMessage.style.display = 'block'; //  The success message
+        form.reset(); 
     });
 });
 
